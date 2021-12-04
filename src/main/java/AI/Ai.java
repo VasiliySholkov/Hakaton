1576b7d9c928167e6ce82d31a2ebd741d02ffc66
tarosir <torin901@gmail.com>
2021-12-04 22:40:35 +0300

    1

---
+++ src/main/java/AI/Ai.java
@@ -0,0 +1,22 @@
+package AI;
+
+
+import java.util.Random;
+
+public class Ai {
+
+        Random random = new Random();
+
+        public  int[][] fleetCreate(int[][] battleplan) {
+
+            int startingPositionX= random.nextInt(10);
+            int startingPositionY = random.nextInt(10);
+
+
+
+
+            return battleplan;
+        }
+
+    }
+
