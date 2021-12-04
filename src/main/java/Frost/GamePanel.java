f3ffa94663240c5ddb0a32ef0fbcb4bd333f80f4
AliaksandrMoroz <89323365+AliaksandrMoroz@users.noreply.github.com>
2021-12-05 01:26:22 +0300

    start

--- src/main/java/Frost/GamePanel.java
+++ src/main/java/Frost/GamePanel.java
@@ -2,45 +2,55 @@ package Frost;
 
 
 import java.io.File;
+import java.util.Arrays;
 
 public class GamePanel {
     File file = new File("");
-    public String[] pole1 = new String[]{
-            "   A B C D I F G H I J ",
-            "1 | | | | | | | | | | |",
-            "2 | | | | | | | | | | |",
-            "3 | | | | | | | | | | |",
-            "4 | | | | | | | | | | |",
-            "5 | | | | | | | | | | |",
-            "6 | | | | | | | | | | |",
-            "7 | | | | | | | | | | |",
-            "8 | | | | | | | | | | |",
-            "9 | | | | | | | | | | |",
-            "10| | | | | | | | | | |",
+    public String[][] pole1 = new String[][]{
+            { " "," "," "," "," "," "," "," "," "," ",},
+            { " "," "," "," "," "," "," "," "," "," ",},
+            { " "," "," "," "," "," "," "," "," "," ",},
+            { " "," "," "," "," "," "," "," "," "," ",},
+            { " "," "," "," "," "," "," "," "," "," ",},
+            { " "," "," "," "," "," "," "," "," "," ",},
+            { " "," "," "," "," "," "," "," "," "," ",},
+            { " "," "," "," "," "," "," "," "," "," ",},
+            { " "," "," "," "," "," "," "," "," "," ",},
+            { " "," "," "," "," "," "," "," "," "," ",},
+
     };
-    public String[] pole2 = new String[]{
-            "   A B C D I F G H I J ",
-            "1 | | | | | | | | | | |",
-            "2 | | | | | | | | | | |",
-            "3 | | | | | | | | | | |",
-            "4 | | | | | | | | | | |",
-            "5 | | | | | | | | | | |",
-            "6 | | | | | | | | | | |",
-            "7 | | | | | | | | | | |",
-            "8 | | | | | | | | | | |",
-            "9 | | | | | | | | | | |",
-            "10| | | | | | | | | | |",
+    public String[][] pole2 = new String[][]{
+            { " "," "," "," "," "," "," "," "," "," ",},
+            { " "," "," "," "," "," "," "," "," "," ",},
+            { " "," "," "," "," "," "," "," "," "," ",},
+            { " "," "," "," "," "," "," "," "," "," ",},
+            { " "," "," "," "," "," "," "," "," "," ",},
+            { " "," "," "," "," "," "," "," "," "," ",},
+            { " "," "," "," "," "," "," "," "," "," ",},
+            { " "," "," "," "," "," "," "," "," "," ",},
+            { " "," "," "," "," "," "," "," "," "," ",},
+            { " "," "," "," "," "," "," "," "," "," ",},
     };
 
-   public void showPole(){
-       int count=0;
-        for (String el:
-             this.pole1) {
-            System.out.print(el);
-            System.out.print("\t\t\t\t\t");
-            System.out.println(this.pole2[count++]);
-        }
+   public void showPole(String[][] strings){
+       int count =1;
+       System.out.print("   A B C D I F G H I J");
+       System.out.println();
+  for (int i=0;i<strings.length;i++){
+      if(count<=9){
+      System.out.print(count++ +" |");}
+      else {
+          System.out.print(count+"|");
+      }
+      for (int j = 0; j < strings[i].length; j++) {
+          System.out.print(strings[i][j]+"|");
+      }
+      System.out.println();
+        }count=0;
+
     }
+
+
     public void readFile(){
 
     }
--- src/main/java/Frost/Main.java
+++ src/main/java/Frost/Main.java
@@ -3,7 +3,8 @@ package Frost;
 public class Main {
     public static void main(String[] args) {
         GamePanel gamePanel = new GamePanel();
-        gamePanel.showPole();
+        gamePanel.showPole(gamePanel.pole1);
+        gamePanel.showPole(gamePanel.pole2);
 
 
 
