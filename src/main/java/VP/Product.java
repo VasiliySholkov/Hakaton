5bbf4c0acf2a4a70968a98bb0e0a5ecc1e5db9ce
VasiliySholkov <3730703@bk.ru>
2021-12-05 02:39:07 +0300

    VP

---
+++ src/main/java/VP/Product.java
@@ -0,0 +1,35 @@
+package VP;
+
+public class Product {
+
+
+        private long id;
+        private String name;
+        private int price;
+        private String category;
+
+        static long count=1;
+
+        public Product(String name, int price, String category) {
+            this.id = count++;
+            this.name = name;
+            this.price = price;
+            this.category = category;
+        }
+
+        public long getId() {
+            return id;
+        }
+
+        @Override
+        public String toString() {
+            return "Product{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    ", price=" + price +
+                    ", category='" + category + '\'' +
+                    '}';
+        }
+
+    }
+
